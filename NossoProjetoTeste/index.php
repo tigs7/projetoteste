77d11d50a38cf07ffc7fe25a82e119fa70460c3e
tiago <tiago@tiago-pc>
2017-05-17 23:35:02 -0300

    terceiro commit

--- NossoProjetoTeste/index.php
+++ NossoProjetoTeste/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        echo'Essa é a minha modificação.'
         ?>
     </body>
 </html>
